54d02cee2ddd7f2941ab27906dc9b62141ae8e2c
Tu Bagus Dwi Fikri <tubagusoseng@gmail.com>
2022-01-04 15:20:00 +0700

    Latihan 5.8: Anomali pada Javascript

--- learn.js
+++ learn.js
@@ -104,3 +104,12 @@ console.log(person["lastName"]);
 console.log(person["NPM"]);
 console.log(person["fullName"]());
 
+//anomali pada javascript
+console.log(0.1 + 0.2 == 0.3);
+console.log(1 + "1");
+console.log(1 - "1");
+console.log(1 < 2 < 3);
+console.log(3 > 2 > 1);
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
